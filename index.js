cbf05e3d7d629317efa98efc9bcb22ed239491e8
Josh Brooks <jdbb7d@mail.missouri.edu>
2018-09-13 12:47:09 -0500

    Added app 5

--- index.js
+++ index.js
@@ -25,4 +25,15 @@ var app4 = new Vue({
 			{ text: 'Build something awesome' }
 		]
 	}
+})
+var app5 = new Vue({
+	el: '#app-5',
+	data: {
+		message: 'Hello Vue.js!'
+	},
+	methods: {
+		reverseMessage: function () {
+		this.message = this.message.split('').reverse().join('')
+		}
+	}
 });
