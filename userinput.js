1c43fe90f44611cc0a388c910aebb32a912f6ff9
Josh Brooks <jdbb7d@mail.missouri.edu>
2018-09-14 14:49:30 -0500

    Update userinput.js

--- userinput.js
+++ userinput.js
@@ -1,6 +1,15 @@
-new Vue({
+var text new Vue({
+  el: '#example-1',
+})
+var multitext new Vue({
+  el: '#example-2',
+})
+var checkbox new Vue({
+  el: '#example',
+})
+var checknames new Vue({
   el: '#example-3',
   data: {
     checkedNames: []
   }
-})
+});
